c63683f56b778ae54d1bf940bc902fd9f585c607
takada-s <shingot@ricsec.co.jp>
2022-03-08 15:47:42 +0900

    impl outdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -l
-echo "Hello $1"
+echo "CWD=$(pwd)"
+OUTDIR="$1"
+echo "OUTDIR=${OUTDIR}"
+mkdir -p $OUTDIR
+echo "file 1" > ${OUTDIR}/1.txt
+echo "file 2" > ${OUTDIR}/2.txt
+echo "file 3" > ${OUTDIR}/3.txt
+
+echo "Hello $2"
 time=$(date)
 echo "::set-output name=time::$time"
 
